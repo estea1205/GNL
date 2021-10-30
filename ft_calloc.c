ad947abd3880e6212431712330c316fd3790a373
Esther Teh <eteh@u81z01s06.42kl.edu.my>
2021-10-30 22:03:44 +0800

    calloc

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eteh <eteh@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 17:08:53 by eteh              #+#    #+#             */
+/*   Updated: 2021/10/22 17:10:23 by eteh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc (count * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero (ptr, count * size);
+	return (ptr);
+}
