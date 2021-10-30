28ef6aad4a48e2c08529f0db06ff546cf6e40138
Esther Teh <eteh@u81z01s06.42kl.edu.my>
2021-10-30 22:08:40 +0800

    strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eteh <eteh@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/02 17:08:34 by eteh              #+#    #+#             */
+/*   Updated: 2021/10/22 11:07:02 by eteh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (*needle == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (needle[j] == haystack[i + j] && (i + j) < len)
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *)haystack + i);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
+/*	
+int	main (void)
+{
+	char a[] = "Hello World";
+	char b[] = "Wor";
+
+	printf("selfmade:%s\n", ft_strnstr(a,b,11));
+	printf("target:%s\n", strnstr(a,b,11));
+	return (0);
+}
+*/
