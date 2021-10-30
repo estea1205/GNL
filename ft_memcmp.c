e92b9c65c7e1e49e0f075405efc6115b630fee65
Esther Teh <eteh@u81z01s06.42kl.edu.my>
2021-10-30 22:05:42 +0800

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eteh <eteh@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/02 17:54:04 by eteh              #+#    #+#             */
+/*   Updated: 2021/10/22 11:22:43 by eteh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*str1;
+	const unsigned char	*str2;
+	size_t				i;
+
+	i = 0;
+	str1 = (const unsigned char *)s1;
+	str2 = (const unsigned char *)s2;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
+/*
+int	main (void)
+{
+	unsigned char a[] = "hello";
+	unsigned char b[] = "hemlo";
+
+	printf("selfmade:%d\n",ft_memcmp(a,b,3));
+	printf("target:%d\n",memcmp(a,b,3));
+	return (0);
+}
+*/
